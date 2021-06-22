29ecd52aeaff56c97268c22d97b9735278138de8
HerrDoctor <samuel.vidal.gallego@gmail.com>
2021-06-22 11:42:09 +0200

    Changed App text

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Delete content here. Edit <code>src/App.js</code> and save to reload.
-          Jelou! There are nothing
+          VERSION 2. Checking whole Workflow
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
